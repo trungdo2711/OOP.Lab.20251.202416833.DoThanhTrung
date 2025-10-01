8a42f40089baef69bd051d3a4104b41ce64d7795
trungdo2711 <ttrung271106@gmail.com>
2025-10-01 19:07:22 +0700

    Add Stars program

---
+++ Stars.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Stars {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of lines: ");
+        int line = scanner.nextInt();
+
+        for (int i = 1; i <= line; i++) {
+            for (int j = 1; j <= line - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
+}
