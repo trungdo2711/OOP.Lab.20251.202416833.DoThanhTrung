179984554e84959ec84c8c47348fe74a3ebec00a
trungdo2711 <ttrung271106@gmail.com>
2025-10-09 16:59:30 +0700

    "OOP Lab02 exercises"

---
+++ Lab02/Cart.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+public class Cart {
+    public static final int MAX_NUMBERS_ORDERED = 20;
+    private ArrayList<DigitalVideoDisc> itemsOrdered = new ArrayList<>();
+
+    public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+        if (itemsOrdered.size() < MAX_NUMBERS_ORDERED) {
+            itemsOrdered.add(disc);
+            System.out.println("The disc \"" + disc.getTitle() + "\" has been added.");
+        } else {
+            System.out.println("The cart is full.");
+        }
+    }
+
+    public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+        boolean removed = itemsOrdered.remove(disc);
+        if (removed) {
+            System.out.println("The disc \"" + disc.getTitle() + "\" has been removed.");
+        } else {
+            System.out.println("The disc \"" + disc.getTitle() + "\" was not found in the cart.");
+        }
+    }
+
+    public double totalCost() {
+        double total = 0;
+        for (DigitalVideoDisc disc : itemsOrdered) {
+            total += disc.getCost();
+        }
+        return total;
+    }
+
+    public void printCart() {
+        System.out.println("***********************CART***********************");
+        System.out.println("Ordered Items:");
+        if (itemsOrdered.isEmpty()) {
+            System.out.println("The cart is empty.");
+        } else {
+            for (int i = 0; i < itemsOrdered.size(); i++) {
+                System.out.println((i + 1) + ". " + itemsOrdered.get(i).toString());
+            }
+        }
+        System.out.println("Total cost: " + String.format("%.2f", totalCost()) + "$");
+        System.out.println("***************************************************");
+    }
+}
